cfc949a02f8696ba7cc3e53df01e0e9037379a11
Monil Panchal <monilpanchal472@gmail.com>
2025-06-17 10:06:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+AOS.init();
+
+new Typed("#typed", {
+  strings: ["Aspiring Data Analyst.", "Quick Learner.", "Excel, Python & Tableau Enthusiast."],
+  typeSpeed: 60,
+  backSpeed: 30,
+  loop: true
+});
+
